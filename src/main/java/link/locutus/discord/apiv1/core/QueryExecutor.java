2d167b5cf63674115e44fcd4c00a3425dc720364
xdnw <jessepaleg@outlook.com>
2023-04-10 11:07:11 +0100

    Update QueryExecutor.java

--- src/main/java/link/locutus/discord/apiv1/core/QueryExecutor.java
+++ src/main/java/link/locutus/discord/apiv1/core/QueryExecutor.java
@@ -7,43 +7,43 @@ import java.io.IOException;
 
 public class QueryExecutor {
 
-  private CacheClient cacheClient = null;
-  private boolean testServerMode;
-  private boolean enableCache;
+    private final boolean testServerMode;
+    private final boolean enableCache;
+    private CacheClient cacheClient = null;
 
-  public QueryExecutor(boolean enableCache, boolean testServerMode, int cacheMaxSize, long cacheRetainTime) {
-    this.enableCache = enableCache;
-    this.testServerMode = testServerMode;
-    if (this.enableCache)
-      cacheClient = new CacheClient(cacheMaxSize, cacheRetainTime);
-  }
+    public QueryExecutor(boolean enableCache, boolean testServerMode, int cacheMaxSize, long cacheRetainTime) {
+        this.enableCache = enableCache;
+        this.testServerMode = testServerMode;
+        if (this.enableCache)
+            cacheClient = new CacheClient(cacheMaxSize, cacheRetainTime);
+    }
 
-  public Entity execute(ApiQuery apiQuery) throws IOException {
-    apiQuery.buildUrlStr(testServerMode);
-    String url = apiQuery.getUrlStr();
-    if (this.enableCache && cacheClient.contains(url))
-      return getFromCache(url);
-    else
-      return getFromSource(apiQuery);
-  }
+    public Entity execute(ApiQuery apiQuery) throws IOException {
+        apiQuery.buildUrlStr(testServerMode);
+        String url = apiQuery.getUrlStr();
+        if (this.enableCache && cacheClient.contains(url))
+            return getFromCache(url);
+        else
+            return getFromSource(apiQuery);
+    }
 
-  private Entity getFromCache(String url) {
-    return cacheClient.getIfExists(url);
-  }
+    private Entity getFromCache(String url) {
+        return cacheClient.getIfExists(url);
+    }
 
-  private Entity getFromSource(ApiQuery apiQuery) throws IOException {
-    Entity entity = apiQuery.fetchAPI().getEntity();
-    if (this.enableCache)
-      cacheClient.add(apiQuery.getUrlStr(), entity);
-    return entity;
-  }
+    private Entity getFromSource(ApiQuery apiQuery) throws IOException {
+        Entity entity = apiQuery.fetchAPI().getEntity();
+        if (this.enableCache)
+            cacheClient.add(apiQuery.getUrlStr(), entity);
+        return entity;
+    }
 
-  public CacheClient getCacheClient() {
-    return cacheClient;
-  }
+    public CacheClient getCacheClient() {
+        return cacheClient;
+    }
 
-  public void clearCacheClient() {
-    if (cacheClient != null)
-      cacheClient.clear();
-  }
+    public void clearCacheClient() {
+        if (cacheClient != null)
+            cacheClient.clear();
+    }
 }
