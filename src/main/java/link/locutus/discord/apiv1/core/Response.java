55c8b0d436f1c13da10c6788f3df474a5d1bd547
xdnw <jessepaleg@outlook.com>
2023-04-10 11:05:29 +0100

    Update Response.java

--- src/main/java/link/locutus/discord/apiv1/core/Response.java
+++ src/main/java/link/locutus/discord/apiv1/core/Response.java
@@ -12,40 +12,39 @@ import java.lang.reflect.Type;
 
 public class Response<T extends Entity> {
 
-  private String jsonStr;
-  private Gson gson = new Gson();
-  private T t;
-  private String url;
+    private final String jsonStr;
+    private final Gson gson = new Gson();
+    private final T t;
+    private final String url;
 
-  public Response(String jsonStr, T t, String url) {
-    this.jsonStr = jsonStr;
-    this.t = t;
-    this.url = Utility.obfuscateApiKey(url);
-  }
+    public Response(String jsonStr, T t, String url) {
+        this.jsonStr = jsonStr;
+        this.t = t;
+        this.url = Utility.obfuscateApiKey(url);
+    }
 
-  public String getJson() {
-    return jsonStr;
-  }
+    public String getJson() {
+        return jsonStr;
+    }
 
-  public T getEntity() throws JsonSyntaxException {
-      JsonElement jsonElement = new JsonParser().parse(jsonStr);
-      JsonObject jsonObject = jsonElement.getAsJsonObject();
+    public T getEntity() throws JsonSyntaxException {
+        JsonElement jsonElement = new JsonParser().parse(jsonStr);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
 
       if(!t.getClass().getName().equals(Wars.class.getName()) || !t.getClass().getName().equals(Alliances.class.getName()) ||
           !t.getClass().getName().equals(Nations.class.getName())) {
         if (jsonObject.has("error") || jsonObject.has("error_message") ||
-            jsonObject.has("general_message") || jsonObject.has("message")) {
-          if(jsonObject.has("general_message"))
-            throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("general_message").getAsString(), url);
-          else if(jsonObject.has("error"))
-            throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("error").getAsString(), url);
-          else if(jsonObject.has("error_message"))
-            throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("error_message").getAsString(), url);
-          else if(jsonObject.has("message"))
-            throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("message").getAsString(), url);
+                jsonObject.has("general_message") || jsonObject.has("message")) {
+            if (jsonObject.has("general_message"))
+                throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("general_message").getAsString(), url);
+            else if (jsonObject.has("error"))
+                throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("error").getAsString(), url);
+            else if (jsonObject.has("error_message"))
+                throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("error_message").getAsString(), url);
+            else if (jsonObject.has("message"))
+                throw new PoliticsAndWarAPIException("Unsuccessful API request. Error Received: " + jsonObject.get("message").getAsString(), url);
         }
-      }
-      Type type = TypeToken.get(t.getClass()).getType();
-      return gson.fromJson(jsonStr,type);
-  }
+        Type type = TypeToken.get(t.getClass()).getType();
+        return gson.fromJson(jsonStr, type);
+    }
 }
