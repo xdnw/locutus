e9c192b21b462591681e913a34a047d7b9ee4618
xdnw <jessepaleg@outlook.com>
2023-04-19 06:23:18 +0100

    Update Loot.java

--- src/main/java/link/locutus/discord/commands/war/Loot.java
+++ src/main/java/link/locutus/discord/commands/war/Loot.java
@@ -4,6 +4,7 @@ import link.locutus.discord.Locutus;
 import link.locutus.discord.commands.manager.Command;
 import link.locutus.discord.commands.manager.CommandCategory;
 import link.locutus.discord.commands.manager.v2.binding.bindings.PrimitiveBindings;
+import link.locutus.discord.commands.manager.v2.impl.discord.DiscordChannelIO;
 import link.locutus.discord.commands.manager.v2.impl.pw.binding.PWBindings;
 import link.locutus.discord.commands.manager.v2.impl.pw.commands.UtilityCommands;
 import link.locutus.discord.config.Settings;
@@ -68,6 +69,6 @@ public class Loot extends Command {
         }
 
         NationOrAlliance nationOrAlliance = PWBindings.nationOrAlliance(args.get(0));
-        return UtilityCommands.loot(me, nationOrAlliance, score, flags.contains('p'));
+        return UtilityCommands.loot(new DiscordChannelIO(event), me, nationOrAlliance, score, flags.contains('p'));
     }
 }
