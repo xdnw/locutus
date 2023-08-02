c89c39e7c529da4a1a2149e1ba4a129741a53d96
xdnw <jessepaleg@outlook.com>
2023-08-02 13:49:26 +0100

    Add ocr api

--- src/main/java/link/locutus/discord/web/test/TestCommands.java
+++ src/main/java/link/locutus/discord/web/test/TestCommands.java
@@ -31,21 +31,8 @@ public class TestCommands {
 
     @Command(desc = "Get the text from a discord image\n" +
             "It is recommended to crop the image first")
-    public String ocr(String discordImageUrl, @Default ImageType type) {
-        if (type == null) type = ImageType.CLEAR;
-        String text = ImageUtil.getText(discordImageUrl, type);
-        return "```\n" +text + "\n```\n" +
-        """
-        OCR stands for "Optical Character Recognition," and it is a technology that converts images of text into machine-readable and editable text.
-        This command does no image processing. The following is recommended:
-        - Use Clear Images: Ensure that the text you want to extract is clear, level, well-lit, and not blurry.
-        - Crop Unnecessary Parts: If the image contains unnecessary areas (like borders), crop the image to focus only on the text you need.
-        - Use High-Quality Image Formats: Whenever possible, use high-quality formats like PNG or TIFF for better OCR results.
-        - Avoid Fancy Fonts: Stick to standard, easily recognizable fonts. Avoid using decorative or handwritten-style fonts that OCR might struggle with. 
-                        
-        This command uses Tesseract: <https://github.com/tesseract-ocr/tesseract>
-        For a GUI tool, see: <https://www.naps2.com/>
-        """;
-
+    public String ocr(String discordImageUrl) {
+        String text = ImageUtil.getText(discordImageUrl);
+        return "```\n" +text + "\n```\n";
     }
 }
