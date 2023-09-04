5bbb311b330e980ba8b75ca28121cf03ed8f8475
xdnw <jessepaleg@outlook.com>
2023-09-04 13:55:59 +0100

    wip loan requests

--- src/main/java/link/locutus/discord/commands/manager/v2/impl/pw/commands/LoanCommands.java
+++ src/main/java/link/locutus/discord/commands/manager/v2/impl/pw/commands/LoanCommands.java
@@ -1,9 +1,12 @@
 package link.locutus.discord.commands.manager.v2.impl.pw.commands;
 
 import link.locutus.discord.commands.manager.v2.binding.annotation.Binding;
+import link.locutus.discord.commands.manager.v2.binding.annotation.Command;
 import link.locutus.discord.commands.manager.v2.binding.annotation.Me;
 import link.locutus.discord.commands.manager.v2.binding.annotation.Timediff;
+import link.locutus.discord.commands.manager.v2.command.IMessageIO;
 import link.locutus.discord.commands.manager.v2.impl.discord.permission.RolePermission;
+import link.locutus.discord.config.Messages;
 import link.locutus.discord.db.GuildDB;
 import link.locutus.discord.db.entities.DBLoan;
 import link.locutus.discord.db.entities.DBNation;
@@ -14,6 +17,7 @@ import link.locutus.discord.apiv1.enums.ResourceType;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import rocker.guild.ia.message;
@@ -21,6 +25,23 @@ import rocker.guild.ia.message;
 import java.util.Map;
 
 public class LoanCommands {
+
+    /*
+Loan amount (money, or resources)
+time_period
+purpose
+collateral
+cosigner
+additional_notes
+
+A note the user wants to add to the request
+     */
+    @Command
+    public String requestInfo(@Me IMessageIO io, @Me GuildDB db, @Me User user, @Me DBNation nation) {
+        String msg = Messages.LOAN_REQUEST + "\n\nTo open a request, use: TODO CM REF;
+
+    }
+
 
 //    @Binding
 //    @RolePermission(value= Roles.ECON)
