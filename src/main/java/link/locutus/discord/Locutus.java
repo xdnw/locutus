5bf53b0752b7bc2c2cd3f568fea2c15099aaa4f0
xdnw <jessepaleg@outlook.com>
2023-10-14 15:59:26 +0100

    Info logging

--- src/main/java/link/locutus/discord/Locutus.java
+++ src/main/java/link/locutus/discord/Locutus.java
@@ -83,6 +83,11 @@ import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.apache.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
@@ -138,7 +143,23 @@ public final class Locutus extends ListenerAdapter {
         }
     }
 
+    private void setInfoLogging() {
+        org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
+//        Configurator.setRootLevel(org.apache.logging.log4j.Level.INFO);
+
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        Configuration config = ctx.getConfiguration();
+        LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+        loggerConfig.setLevel(org.apache.logging.log4j.Level.INFO);
+        ctx.updateLoggers();
+
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(ch.qos.logback.classic.Level.INFO);
+    }
+
     private Locutus() throws SQLException, ClassNotFoundException, LoginException, InterruptedException, NoSuchMethodException {
+        setInfoLogging();
+
         if (INSTANCE != null) throw new IllegalStateException("Already running.");
         INSTANCE = this;
         long start = System.currentTimeMillis();
