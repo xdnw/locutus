7d2104e94aecf751c275b8bf456e012e3dc56455
xdnw <jessepaleg@outlook.com>
2023-10-18 04:16:04 +0100

    war equals

--- src/main/java/link/locutus/discord/db/WarDB.java
+++ src/main/java/link/locutus/discord/db/WarDB.java
@@ -1655,16 +1655,21 @@ public class WarDB extends DBMainV2 {
         List<DBWar> newWars = new ArrayList<>();
         Set<Integer> expectedIdsSet = expectedIds == null ? null : expectedIds instanceof Set ? (Set<Integer>) expectedIds : new ObjectOpenHashSet<>(expectedIds);
         Set<Integer> idsFetched = dbWars.stream().map(DBWar::getWarId).collect(Collectors.toSet());
-        Set<Integer> newWarIds = new LinkedHashSet<>();
 
         for (DBWar war : dbWars) {
             DBWar existing = warsById.get(war);
+            if (war.warId == 1828876) {
+                System.out.println("Found war 1828876 " + war + " | " + existing + " | ");
+            }
 
             if ((existing == null && !war.isActive()) || (existing != null && (war.getStatus() == existing.getStatus() || !existing.isActive()))) continue;
 
+            if (war.warId == 1828876) {
+                System.out.println("Found 2 war 1828876 " + war + " | " + existing + " | " + war.getStatus() + " | " + (existing != null ? existing.getStatus() : "null"));
+            }
+
             prevWars.add(existing == null ? null : new DBWar(existing));
             newWars.add(war);
-            newWarIds.add(war.getWarId());
 
             if (existing == null && war.getDate() > System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(15) && war.getStatus() == WarStatus.ACTIVE) {
                 Locutus.imp().getNationDB().setNationActive(war.getAttacker_id(), war.getDate(), eventConsumer);
