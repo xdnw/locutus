0ed718c2a8da99da71684a44755942bd002dcf6f
test <jessepaleg@outlook.com>
2025-06-30 15:31:08 +1000

    wip

--- src/main/java/link/locutus/discord/util/io/BitBuffer.java
+++ src/main/java/link/locutus/discord/util/io/BitBuffer.java
@@ -1,5 +1,7 @@
 package link.locutus.discord.util.io;
 
+import link.locutus.discord.util.IOUtil;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
@@ -99,16 +101,17 @@ public class BitBuffer {
 
     private long buffer;
     private int bitsInBuffer;
-    private final ByteBuffer byteBuffer;
+    private byte[] byteArray;
+    private int offset;
 
     public BitBuffer(int capacity) {
-        this(ByteBuffer.wrap(new byte[capacity]).order(ByteOrder.LITTLE_ENDIAN));
+        this(new byte[capacity]);
     }
 
-    private BitBuffer(ByteBuffer byteBuffer) {
+    private BitBuffer(byte[] byteBuffer) {
         this.buffer = 0L;
         this.bitsInBuffer = 0;
-        this.byteBuffer = byteBuffer;
+        this.byteArray = byteBuffer;
     }
 
     public void writeBits(long value, int count) {
@@ -143,7 +146,10 @@ public class BitBuffer {
         } else {
             int remainingCount = count - bitsInBuffer;
             result = buffer & MASK[bitsInBuffer];
-            buffer = byteBuffer.getLong();
+
+            buffer = IOUtil.readLong(byteArray, offset);
+            offset += Long.BYTES;
+
             result |= (buffer & MASK[remainingCount]) << bitsInBuffer;
             buffer >>>= remainingCount;
             bitsInBuffer = 64 - remainingCount;
@@ -153,17 +159,19 @@ public class BitBuffer {
     }
 
     private void flush() {
-        byteBuffer.putLong(buffer);
+        IOUtil.writeLong(byteArray, buffer, offset);
+        offset += Long.BYTES;
+
         buffer = 0;
         bitsInBuffer = 0;
     }
 
     public byte[] getWrittenBytes() {
         int numBytesInBuffer = (bitsInBuffer + 7) / 8;
-        byte[] bytes = new byte[byteBuffer.position() + numBytesInBuffer];
-        System.arraycopy(byteBuffer.array(), 0, bytes, 0, byteBuffer.position());
+        byte[] bytes = new byte[offset + numBytesInBuffer];
+        System.arraycopy(byteArray, 0, bytes, 0, offset);
         if (numBytesInBuffer > 0) {
-            int startIndex = byteBuffer.position();
+            int startIndex = offset;
             for (int i = 0; i < numBytesInBuffer; i++) {
                 bytes[startIndex + i] = (byte) (buffer & 0xFF);
                 buffer >>>= 8;
