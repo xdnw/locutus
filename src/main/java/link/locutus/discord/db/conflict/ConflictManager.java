e8b3e3fd2a6a9971c885b9aa6d0db3aa0bca6424
test <jessepaleg@outlook.com>
2025-10-21 17:31:26 +1100

    Update ConflictManager.java

--- src/main/java/link/locutus/discord/db/conflict/ConflictManager.java
+++ src/main/java/link/locutus/discord/db/conflict/ConflictManager.java
@@ -535,7 +535,7 @@ public class ConflictManager {
             Locutus.imp().getRepeatingTasks().addTask("Conflict Website", () -> {
                 if (!conflictsLoaded) return;
                 pushDirtyConflicts();
-            }, Settings.INSTANCE.TASKS.WAR_STATS_PUSH_INTERVAL, TimeUnit.MINUTES);
+            }, Settings.INSTANCE.TASKS.WAR_STATS_PUSH_INTERVAL, TimeUnit.SECONDS);
         });
     }
 
